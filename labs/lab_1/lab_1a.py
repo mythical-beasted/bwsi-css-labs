d4a41434e7bdae0841cb80697cd3700bf4fea829
mythical-beasted <chrislittot@gmail.com>
2026-02-22 16:20:08 -0500

    Added introduction

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Chris" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Excited to learn more!")
 
 if __name__ == "__main__":
     main()
